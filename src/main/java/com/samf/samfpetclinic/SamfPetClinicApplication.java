863824bed1995882b58702e6a0db9ccba5453e6c
Shane <shanefeehily9@hotmail.com>
2018-10-24 15:59:47 +0100

    Initial check in

---
+++ src/main/java/com/samf/samfpetclinic/SamfPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.samf.samfpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamfPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamfPetClinicApplication.class, args);
+	}
+}

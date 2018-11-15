d40ad0ae1fffa30876becc72d9936c617f6377bd
Shane <shanefeehily9@hotmail.com>
2018-11-15 15:29:05 +0000

    Refactored and added base CRUD Interface. Closes #11

--- pet-clinic-data/src/main/java/com/samf/samfpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/samf/samfpetclinic/services/VetService.java
@@ -2,14 +2,6 @@ package com.samf.samfpetclinic.services;
 
 import com.samf.samfpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 
 }

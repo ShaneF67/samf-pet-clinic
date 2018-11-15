6780e8bcb8d2bf8bb0570d7ea2a96da54a34ee51
Shane <shanefeehily9@hotmail.com>
2018-11-15 17:21:23 +0000

    Added vets index page. Closes #7.

---
+++ pet-clinic-web/src/main/java/com/samf/samfpetclinic/controllers/VetController.java
@@ -0,0 +1,15 @@
+package com.samf.samfpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vets")
+public class VetController {
+
+
+    @RequestMapping({"", "/index"})
+    public String listVets() {
+        return "vets/index";
+    }
+}

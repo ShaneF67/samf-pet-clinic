8dfd795e3540440263f4c16306d776ebe95fa449
Shane <shanefeehily9@hotmail.com>
2018-11-15 12:50:23 +0000

    Added service interfaces. Closes #5

---
+++ pet-clinic-data/src/main/java/com/samf/samfpetclinic/services/OwnerService.java
@@ -0,0 +1,17 @@
+package com.samf.samfpetclinic.services;
+
+import com.samf.samfpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+
+}
---
+++ pet-clinic-data/src/main/java/com/samf/samfpetclinic/services/PetService.java
@@ -0,0 +1,15 @@
+package com.samf.samfpetclinic.services;
+
+import com.samf.samfpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+
+}

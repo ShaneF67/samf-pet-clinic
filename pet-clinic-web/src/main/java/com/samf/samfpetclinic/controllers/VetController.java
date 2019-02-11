34b0f29d56fe97f7dda3d08d6c65009ccb28f5b5
Shane <shanefeehily9@hotmail.com>
2019-02-11 11:35:42 -0500

    Added list of vets to web page. Closes #15

--- pet-clinic-web/src/main/java/com/samf/samfpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/samf/samfpetclinic/controllers/VetController.java
@@ -1,15 +1,25 @@
 package com.samf.samfpetclinic.controllers;
 
+import com.samf.samfpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
+    private final VetService vetService;
 
-    @RequestMapping({"", "/index"})
-    public String listVets() {
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/index", "vets"})
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }

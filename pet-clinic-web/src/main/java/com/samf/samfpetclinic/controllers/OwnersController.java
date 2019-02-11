02b40a1cd7e416981716a5fc6c95ec7066aca9f2
Shane <shanefeehily9@hotmail.com>
2019-02-11 11:24:43 -0500

    Added list of owners to web page. Closes #16

--- pet-clinic-web/src/main/java/com/samf/samfpetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/com/samf/samfpetclinic/controllers/OwnersController.java
@@ -1,15 +1,23 @@
 package com.samf.samfpetclinic.controllers;
 
+import com.samf.samfpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnersController {
 
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
 
     @RequestMapping({"", "/index"})
-    public String getOwners() {
+    public String getOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }

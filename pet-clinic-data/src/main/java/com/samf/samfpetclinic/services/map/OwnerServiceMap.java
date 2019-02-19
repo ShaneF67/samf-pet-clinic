827841cc9282ef26c8443aa606250fdf4667b133
Shane <shanefeehily9@hotmail.com>
2019-02-18 22:15:03 -0500

    Added Pets and Contact info to startup. Closes #33.

--- pet-clinic-data/src/main/java/com/samf/samfpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/samf/samfpetclinic/services/map/OwnerServiceMap.java
@@ -1,13 +1,23 @@
 package com.samf.samfpetclinic.services.map;
 
 import com.samf.samfpetclinic.model.Owner;
+import com.samf.samfpetclinic.model.Pet;
 import com.samf.samfpetclinic.services.OwnerService;
+import com.samf.samfpetclinic.services.PetService;
+import com.samf.samfpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
 
 
     @Override
@@ -21,8 +31,26 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     }
 
     @Override
-    public Owner save(Owner object) {
-        return super.save(object);
+    public Owner save(Owner owner) {
+        if (owner != null) {
+            if (owner.getPets() != null) {
+
+                owner.getPets().forEach(pet -> {
+                    if (pet.getPetType() != null) {
+                        pet.setPetType(petTypeService.save(pet.getPetType()));
+                    } else {
+                        throw new RuntimeException("Pet type is required");
+                    }
+
+                    if (pet.getId() == null) {
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+            return super.save(owner);
+        }
+        return null;
     }
 
     @Override
--- pet-clinic-web/src/main/java/com/samf/samfpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/samf/samfpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.samf.samfpetclinic.bootstrap;
 
 import com.samf.samfpetclinic.model.Owner;
+import com.samf.samfpetclinic.model.Pet;
 import com.samf.samfpetclinic.model.PetType;
 import com.samf.samfpetclinic.model.Vet;
 import com.samf.samfpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.samf.samfpetclinic.services.PetTypeService;
 import com.samf.samfpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -36,12 +39,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Shane");
         owner1.setLastName("Feehily");
+        owner1.setAddress("Kevinsfort");
+        owner1.setCity("Sligo");
+        owner1.setTelephone("235345345");
+
+        Pet shanesPet = new Pet();
+        shanesPet.setName("Mimzy");
+        shanesPet.setPetType(savedDogType);
+        shanesPet.setBirthDate(LocalDate.now());
+        shanesPet.setOwner(owner1);
+        owner1.getPets().add(shanesPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Ian");
         owner2.setLastName("Smith");
+        owner2.setAddress("Laureles");
+        owner2.setCity("Medellin");
+        owner2.setTelephone("8763876");
+
+        Pet iansPet = new Pet();
+        iansPet.setName("Anna");
+        iansPet.setPetType(savedCatType);
+        iansPet.setBirthDate(LocalDate.now());
+        iansPet.setOwner(owner2);
+        owner2.getPets().add(iansPet);
 
         ownerService.save(owner2);
 

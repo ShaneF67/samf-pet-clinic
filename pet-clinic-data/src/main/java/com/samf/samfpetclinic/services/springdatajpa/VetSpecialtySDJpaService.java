75a7e02e4d33845a94bdad41aa74a3100ad3c14a
ShaneF67 <shanefeehily9@hotmail.com>
2019-03-13 14:27:42 -0500

    Added spring data jpa service for specialties. Closes #44

---
+++ pet-clinic-data/src/main/java/com/samf/samfpetclinic/services/springdatajpa/VetSpecialtySDJpaService.java
@@ -0,0 +1,48 @@
+package com.samf.samfpetclinic.services.springdatajpa;
+
+import com.samf.samfpetclinic.model.Speciality;
+import com.samf.samfpetclinic.repositories.SpecialtyRepository;
+import com.samf.samfpetclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetSpecialtySDJpaService implements SpecialtyService {
+
+    private SpecialtyRepository specialtyRepository;
+
+    public VetSpecialtySDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return specialtyRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return specialtyRepository.save(speciality);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        specialtyRepository.delete(speciality);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialtyRepository.deleteById(id);
+    }
+}

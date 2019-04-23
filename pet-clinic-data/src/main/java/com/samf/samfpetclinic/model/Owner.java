c36b9ed57a45dbd8f723398741e9ae7ff114c35b
ShaneF67 <shanefeehily9@hotmail.com>
2019-04-23 12:45:17 -0500

    Added add and update pet functionality. Closes #56.

--- pet-clinic-data/src/main/java/com/samf/samfpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/samf/samfpetclinic/model/Owner.java
@@ -24,7 +24,7 @@ public class Owner extends Person {
         this.city = city;
         this.telephone = telephone;
 
-        if(pets != null) {
+        if (pets != null) {
             this.pets = pets;
         }
     }
@@ -41,5 +41,23 @@ public class Owner extends Person {
     @Column(name = "pets")
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
+
+    public Pet getPet(String name) {
+        return getPet(name, false);
+    }
+
+    public Pet getPet(String name, boolean ignoreNew) {
+        name = name.toLowerCase();
+        for (Pet pet : pets) {
+            if (!ignoreNew || !pet.isNew()) {
+                String compName = pet.getName();
+                compName = compName.toLowerCase();
+                if (compName.equals(name)) {
+                    return pet;
+                }
+            }
+        }
+        return null;
+    }
 
 }
